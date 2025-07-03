f141039ffb8a2ef134954e5411ac9da1b0c877be
lavanya-goyal-6378 <lavanyagoel2004@gmail.com>
2025-07-03 13:49:48 +0000

    comparisons

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,23 @@
+let num=3
+let negnum=-num
+// console.log(negnum);
+//simple operations like console.log(2+2)
+//2*2 2**3 that is 2power3 ,2/3,2%3,2-2
+let strg1="lavanya"
+let strg2="goyal"
+str3=strg1+strg2
+console.log(str3);
+
+//now the important ones
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2")
+//it uses particular guidelines where we have preferred data types
+
+//trick conversion
+//console.log(+true) it gives 1 only no need to write this bad practice makes code less readable
+
+let getint = 100
+getint++; //like c++ ++a and a++ are different
+console.log(getint);
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+//console.log(2>=3)
+//simple comparisons we alll know
+//while comparing we should have same datatypes
+//otherwise it does not give predictable answers
+//console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(undefined>0);
+//does not give predicatable answers avoid these for confusion 
+
+console.log("2"==2) //it will automatically convert
+//but for === it does strict checking that is it check if same dataype to be compared 
+console.log("2"===2);
